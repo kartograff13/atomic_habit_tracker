cf10db357ba8e0a05695e2ab16e2aa03d89921d0
maks_kras <maksym_krasovskiy@ya.ru>
2026-04-13 12:10:20 +0300

    init startapp habits & rest_framework, rest_framework_sipmlejwt, djoser

--- config/settings.py
+++ config/settings.py
@@ -24,6 +24,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    "rest_framework",
+    "rest_framework_simplejwt",
+    "djoser",
+
+    "habits",
 ]
 
 MIDDLEWARE = [
