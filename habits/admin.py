9759cf0abd5ff77e289094ff2dd83568105693d3
maks_kras <maksym_krasovskiy@ya.ru>
2026-04-13 15:11:11 +0300

    configurate administrations tools

--- habits/admin.py
+++ habits/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from habits.models import Habit, TelegramProfile
+
+
+@admin.register(Habit)
+class HabitAdmin(admin.ModelAdmin):
+    """Административная конфигурация для модели Habit"""
+
+    list_display = ("id", "user", "action", "time", "place", "is_pleasant", "is_public")
+    list_filter = ("is_pleasant", "is_public", "periodicity")
+    search_fields = ("action", "place", "user__username")
+    readonly_fields = ("user",)
+
+
+@admin.register(TelegramProfile)
+class TelegramProfileAdmin(admin.ModelAdmin):
+    """Административная конфигурация для модели TelegramProfile"""
+
+    list_display = ("id", "user", "telegram_chat_id")
+    search_fields = ("user__username", "telegram_chat_id")
